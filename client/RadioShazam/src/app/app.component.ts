58aa3c5b5f55548da7edabbb1541720e89368987
SimonTsankov <simoncankov@gmail.com>
2021-10-26 19:52:02 +0300

    added responsive sidebar

--- client/RadioShazam/src/app/app.component.ts
+++ client/RadioShazam/src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatSidenav} from "@angular/material/sidenav";
+import {BreakpointObserver} from "@angular/cdk/layout";
+import {delay} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RadioShazam';
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  component: string="";
 
-  constructor() {
+  constructor(private router: Router,private observer: BreakpointObserver) {
     console.log("HOORAAY")
   }
+
+
+  ngOnInit(): void {
+
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+          this.sidenav.open();
+      });
+  }
+
+  clickTest() {
+    console.log("clicked!")
+    this.router.navigate(["/login"]);
+  }
+
+  changePage(route: string) {
+    this.router.navigate(["/"+route]);
+  }
+
 }
--- client/RadioShazam/src/app/app.module.ts
+++ client/RadioShazam/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
+import {RouterModule} from "@angular/router";
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  exports: [ RouterModule ],
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
