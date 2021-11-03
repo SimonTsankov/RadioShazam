467300e410f3fd604e3d13e80d29f4ebe0cee82f
SimonTsankov <simoncankov@gmail.com>
2021-11-03 18:17:06 +0200

    login and register components visuals are ready

--- client/RadioShazam/src/app/register/register.component.ts
+++ client/RadioShazam/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import {Router, RouterModule} from "@angular/router";
 
 
 @Component({
@@ -14,7 +15,7 @@ export class RegisterComponent implements OnInit {
   email = new FormControl('', [Validators.email, Validators.required])
   password = new FormControl('', [Validators.required, Validators.minLength(3)])
   passwordConfirm = new FormControl('', [Validators.required, Validators.minLength(3)])
-  username= new FormControl('', [Validators.minLength(4), Validators.required]);
+  username = new FormControl('', [Validators.minLength(4), Validators.required]);
 
   signin: FormGroup = new FormGroup({
     email: this.email,
@@ -24,7 +25,7 @@ export class RegisterComponent implements OnInit {
   });
   hide = true;
 
-  constructor() {
+  constructor(private router: Router) {
 
     // @ts-ignore
     this.email.setValue("");
@@ -39,7 +40,12 @@ export class RegisterComponent implements OnInit {
 
     //TODO: this.registerService.doRegister(email.value, password.value, username)
   }
+
+  changePath(path: string) {
+    this.router.navigate([path])
+  }
 }
+
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
